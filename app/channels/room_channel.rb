1d5ada9efaa0255545372d64d2cbdf0a34fac253
Lemeri02 <9588504@gmail.com>
2020-04-29 04:58:05 +0500

    add channels

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,23 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    logger.info "Subscribed to RoomChannel, roomId: #{params[:roomId]}"
+
+    @room = Room.find(params[:roomId])
+
+    stream_from "room_channel_#{@room.id}"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+    logger.info 'Unsubscribed to RoomChannel'
+  end
+
+  def speak(data)
+    logger.info "RoomChannel, speak: #{data.inspect}"
+
+    MessageService.new(
+      body: data['message'], user: current_user, room: @room
+      ).perform
+  end
+end
