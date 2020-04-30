c968d9e503a8c4463a5c0925e08a8af46a1dd1c1
Lemeri02 <9588504@gmail.com>
2020-04-30 17:48:42 +0500

    fix duplicates online users

--- app/channels/appearance_channel.rb
+++ app/channels/appearance_channel.rb
@@ -2,18 +2,35 @@ class AppearanceChannel < ApplicationCable::Channel
   def subscribed
     stream_from "appearance_channel"
 
-    current_user.update!(status: true)
+    current_user_appear
 
-    appear
+    update_online_status
   end
 
   def unsubscribed
-    current_user.update!(status: false)
+    current_user_away
 
-    appear
+    update_online_status
   end
 
-  def appear
+  def away
+    current_user_away
+
+    update_online_status
+  end
+
+  def update_online_status
     ActionCable.server.broadcast "appearance_channel", user: current_user
+  end
+
+  private
+
+  def current_user_away
+    current_user.update!(status: false)
+  end
+
+
+  def current_user_appear
+    current_user.update!(status: true)
   end
 end
--- app/javascript/channels/appearance_channel.js
+++ app/javascript/channels/appearance_channel.js
@@ -1,37 +1,71 @@
 import consumer from "./consumer"
 
 $(document).on("turbolinks:load", function() {
-  createAppearanceChannel();
-
-  let userNodeStatus = $('#user-list').find(`[data-user-status=false]`);
-  userNodeStatus.remove();
-});
-
-const createAppearanceChannel = () => {
   consumer.subscriptions.create({channel: "AppearanceChannel"}, {
+    initialized() {
+      this.update = this.update.bind(this)
+    },
+
     connected() {
+      this.install()
+      this.update()
     },
 
     disconnected() {
+      this.uninstall()
+    },
+
+    rejected() {
+      this.uninstall()
+    },
+
+    update() {
+      this.documentIsActive ? this.update_online_status() : this.away()
+    },
+
+    update_online_status() {
+      this.perform("update_online_status")
+    },
+
+    away() {
+      this.perform("away")
+      this.uninstall()
     },
 
     received(data) {
-      let user = data.user;
-      let nickname = user.nickname;
-      let status = data.user.status;
-      let userNodeId = $('#user-list').find(`[data-user-id=${user.id}]`);
+      let { user } = data;
+      let { nickname, status, id } = user;
+      let userNodeId = $('#user-list').find(`[data-user-id=${id}]`);
 
       if (!status){
         userNodeId.remove();
       };
 
-      if (status && (userNodeId.length != 1)){
+      if (status && userNodeId.length != 1){
         $('#user-list').append(`
-          <li data-user-id="${user.id}" data-user-status="${user.status}">
+          <li data-user-id="${id}" data-user-status="${status}">
             <b>${nickname}</b>
             <span class="spinner-grow spinner-grow-sm text-success">&#183;</span>
           </li>`);
       };
     },
+
+    install() {
+      window.addEventListener("focus", this.update)
+      window.addEventListener("blur", this.update)
+      document.addEventListener("turbolinks:load", this.update)
+      document.addEventListener("visibilitychange", this.update)
+    },
+
+    uninstall() {
+      window.removeEventListener("focus", this.update)
+      window.removeEventListener("blur", this.update)
+      document.removeEventListener("turbolinks:load", this.update)
+      document.removeEventListener("visibilitychange", this.update)
+    },
+
+    get documentIsActive() {
+      return document.visibilityState == "visible" && document.hasFocus()
+    },
   });
-};
+});
