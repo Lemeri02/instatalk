64c7c57c7992796e50bd3b64e9425a2c3465fa60
Lemeri02 <9588504@gmail.com>
2020-05-01 00:51:39 +0500

    fix duplicate users

--- app/javascript/channels/appearance_channel.js
+++ app/javascript/channels/appearance_channel.js
@@ -1,71 +1,69 @@
 import consumer from "./consumer"
 
-$(document).on("turbolinks:load", function() {
-  consumer.subscriptions.create({channel: "AppearanceChannel"}, {
-    initialized() {
-      this.update = this.update.bind(this)
-    },
+consumer.subscriptions.create({channel: "AppearanceChannel"}, {
+  initialized() {
+    this.update = this.update.bind(this)
+  },
 
-    connected() {
-      this.install()
-      this.update()
-    },
+  connected() {
+    this.install()
+    this.update()
+  },
 
-    disconnected() {
-      this.uninstall()
-    },
+  disconnected() {
+    this.uninstall()
+  },
 
-    rejected() {
-      this.uninstall()
-    },
+  rejected() {
+    this.uninstall()
+  },
 
-    update() {
-      this.documentIsActive ? this.update_online_status() : this.away()
-    },
+  update() {
+    this.documentIsActive ? this.update_online_status() : this.away()
+  },
 
-    update_online_status() {
-      this.perform("update_online_status")
-    },
+  update_online_status() {
+    this.perform("update_online_status")
+  },
 
-    away() {
-      this.perform("away")
-      this.uninstall()
-    },
+  away() {
+    this.perform("away")
+    this.uninstall()
+  },
 
-    received(data) {
-      let { user } = data;
-      let { nickname, status, id } = user;
-      let userNodeId = $('#user-list').find(`[data-user-id=${id}]`);
+  received(data) {
+    let { user } = data;
+    let { nickname, status, id } = user;
+    let userNodeId = $('#user-list').find(`[data-user-id=${id}]`);
 
-      if (!status){
-        userNodeId.remove();
-      };
+    if (!status){
+      userNodeId.remove();
+    };
 
-      if (status && userNodeId.length != 1){
-        $('#user-list').append(`
-          <li data-user-id="${id}" data-user-status="${status}">
-            <b>${nickname}</b>
-            <span class="spinner-grow spinner-grow-sm text-success">&#183;</span>
-          </li>`);
-      };
-    },
+    if (status && userNodeId.length != 1){
+      $('#user-list').append(`
+        <li data-user-id="${id}" data-user-status="${status}">
+          <b>${nickname}</b>
+          <span class="spinner-grow spinner-grow-sm text-success">&#183;</span>
+        </li>`);
+    };
+  },
 
-    install() {
-      window.addEventListener("focus", this.update)
-      window.addEventListener("blur", this.update)
-      document.addEventListener("turbolinks:load", this.update)
-      document.addEventListener("visibilitychange", this.update)
-    },
+  install() {
+    window.addEventListener("focus", this.update)
+    window.addEventListener("blur", this.update)
+    document.addEventListener("turbolinks:load", this.update)
+    document.addEventListener("visibilitychange", this.update)
+  },
 
-    uninstall() {
-      window.removeEventListener("focus", this.update)
-      window.removeEventListener("blur", this.update)
-      document.removeEventListener("turbolinks:load", this.update)
-      document.removeEventListener("visibilitychange", this.update)
-    },
+  uninstall() {
+    window.removeEventListener("focus", this.update)
+    window.removeEventListener("blur", this.update)
+    document.removeEventListener("turbolinks:load", this.update)
+    document.removeEventListener("visibilitychange", this.update)
+  },
 
-    get documentIsActive() {
-      return document.visibilityState == "visible" && document.hasFocus()
-    },
-  });
+  get documentIsActive() {
+    return document.visibilityState == "visible" && document.hasFocus()
+  },
 });
